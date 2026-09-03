f995cf0fdf7317d540c01d38755efe908d6c470b
dzpcorange-a11y <dzpcorange@gmail.com>
2026-09-03 17:40:17 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'repairbox-v24.60-shell-v1';
+const APP_SHELL = [
+  './', './index.html', './manifest.webmanifest'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const req=event.request;
+  if(req.method!=='GET') return;
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if(cached) return cached;
+      return fetch(req).then(res=>{
+        if(res && res.ok && new URL(req.url).origin===location.origin){
+          const copy=res.clone(); caches.open(CACHE_NAME).then(c=>c.put(req,copy));
+        }
+        return res;
+      }).catch(()=>cached || caches.match('./index.html'));
+    })
+  );
+});
